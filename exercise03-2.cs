390e26de1d2eefec689417ef25af73829a2fb87f
xingtongli <32304597+xingtongli@users.noreply.github.com>
2021-11-03 20:59:17 -0400

    Add files via upload

---
+++ exercise03-2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace exercise03_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                int max = 500;
+                for (byte i = 0; i < max; i++)
+                {
+                    Console.WriteLine("Hello, world!");
+                }
+            }
+            catch (Exception ex)
+            {
+                # warning The max number exceed the type range  
+            }
+
+
+        }
+    }
+}
